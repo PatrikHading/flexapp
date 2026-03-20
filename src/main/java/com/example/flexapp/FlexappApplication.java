7105d212568c03742e53d8798a0a206460e456dd
hading <hading@gmail.com>
2026-03-20 09:41:20 +0100

    Initial Spring Boot project setup with PostreSQL config

---
+++ src/main/java/com/example/flexapp/FlexappApplication.java
@@ -0,0 +1,13 @@
+package com.example.flexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flexapp/FlexappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
